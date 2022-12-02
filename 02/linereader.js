5be563969b7fb436bc7fe639d86bc2f17e4a0444
Macon Pegram <mpegram@captechconsulting.com>
2022-12-02 09:49:23 -0500

    Refactored and finalized for problem 1 submission.

--- 02/linereader.js
+++ 02/linereader.js
@@ -9,14 +9,10 @@ module.exports = (function() {
             input: fileStream,
             crlfDelay: Infinity,
         });
-        // Note: we use the crlfDelay option to recognize all instances of CR LF
-        // ('\r\n') in input.txt as a single line break.
-
 
         // Each line in input.txt will be successively available here as `line`.
         for await (const line of rl)
             lineProcessor(line, accumulator);
-        console.log('ProcessLineByLine finished');
     }
 
     return this;
