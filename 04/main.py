f078befa7f4f01bbbd05bf7fc3ec411a328d3025
Macon Pegram <mpegram@captechconsulting.com>
2022-12-04 13:53:04 -0500

    Finished up problem 2, and added code comments

--- 04/main.py
+++ 04/main.py
@@ -1,8 +1,20 @@
 import re
 
 
-def parse(line):
-    values = list(map(int, re.split(',|-', line)))
+##############################################################################
+# Assignment:
+#
+# The elves are assigned grids to clean. They've noticed there are some cases where these assignments overlap
+# between the pairs of elves.
+# Problem 1: Find how many of these assignments are COMPLETE overlaps (all of one elf's grids are inside
+# their partner's)
+#
+# Problem 2: Find out how many of these assignments are PARTIAL overlaps
+#############################################################################
+
+# this will take the line and parse it into 2 distinct sets representing the range of numberrs
+def parse(_line):
+    values = list(map(int, re.split(',|-', _line)))
     set1 = set(range(values[0], values[1] + 1))  # +1 to make end of range inclusive
     set2 = set(range(values[2], values[3] + 1))  # +1 to make end of range inclusive
     return [set1, set2]
@@ -11,11 +23,19 @@ def parse(line):
 file1 = open('input.txt', 'r')
 Lines = file1.readlines()
 
-count = 0
-# Strips the newline character
-for line in Lines:
-    elfsets = parse(line)
-    if elfsets[0].issuperset(elfsets[1]) or elfsets[1].issuperset(elfsets[0]):
-        count = count + 1
+answer1count = 0
+answer2count = 0
 
-print(count)
+for line in Lines:
+    elf_sets = parse(line)
+
+    # if either set is a superset of the other, then it meets the criteria for problem one (complete overlap)
+    if elf_sets[0].issuperset(elf_sets[1]) or elf_sets[1].issuperset(elf_sets[0]):
+        answer1count = answer1count + 1
+
+    # if the intersection of the 2 sets has any values at all, it meets the criteria for problem two (partial overlap)
+    if len(elf_sets[0].intersection(elf_sets[1])) > 0:
+        answer2count = answer2count + 1
+
+print("Number of complete overlapping assignments: " + answer1count)
+print("Number of partial overlapping assignemnts: " + answer2count)
