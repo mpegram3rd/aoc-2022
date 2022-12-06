7c008a938e0629664db03e5f49480fd5e53473fa
Macon Pegram <mpegram@captechconsulting.com>
2022-12-06 09:41:25 -0500

    Final formatting and tidying up of code.

--- 06/solution/src/main/kotlin/Main.kt
+++ 06/solution/src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
-import java.io.File
-
-fun main(args: Array<String>) {
+fun main() {
 
     // Read file from classpath (/resources folder)
-    val dataStream: CharArray = {}.javaClass.getResource("input.txt").readText(Charsets.UTF_8).toCharArray()
+    val dataStream: CharArray = {}.javaClass.getResource("input.txt")?.readText(Charsets.UTF_8)!!.toCharArray()
     processData(dataStream, 4, 1)
     processData(dataStream, 14, 2)
 }
@@ -21,21 +19,19 @@ fun processData(dataStream: CharArray, windowSize: Int, problemNum: Int) {
             start += (result - start) + 1 // Move the window to just past the duplicate value
     }
 
-    print ("Problem " + problemNum + ": Start of stream is at position: " + (start + windowSize) + " with marker values: ")
-    for (i in start until start+windowSize) {
+    print("Problem " + problemNum + ": Start of stream is at position: " + (start + windowSize) + " with marker values: ")
+    for (i in start until start + windowSize) {
         print(dataStream[i])
     }
-
     println()
-
 }
+
 fun findDuplicates(dataStream: CharArray, start: Int, windowSize: Int): Int {
-    val letterMap: HashMap<Char, Int> = HashMap<Char, Int> ()
+    val letterMap: HashMap<Char, Int> = HashMap<Char, Int>()
     var index = start
-    var dupeFound = false
     do {
         val currentLetter: Char = dataStream[index]
-        dupeFound = letterMap.containsKey(currentLetter)
+        val dupeFound = letterMap.containsKey(currentLetter)
         // if we find a duplicate we should just skip out and return where the first
         // letter occurred, so we can shift the window past the duplicate letter.
         if (dupeFound) {
