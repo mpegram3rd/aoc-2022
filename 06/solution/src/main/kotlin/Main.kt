8105182452d51ecb17da38c6386ffe5367950ef2
Macon Pegram <mpegram@captechconsulting.com>
2022-12-06 09:27:13 -0500

    Finished problems 1 and 2 and more Gradle/Kotlin gitignore

--- 06/solution/src/main/kotlin/Main.kt
+++ 06/solution/src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    // Read file from classpath (/resources folder)
+    val dataStream: CharArray = {}.javaClass.getResource("input.txt").readText(Charsets.UTF_8).toCharArray()
+    processData(dataStream, 4, 1)
+    processData(dataStream, 14, 2)
+}
+
+fun processData(dataStream: CharArray, windowSize: Int, problemNum: Int) {
+    var start = 0
+    var found = false
+    while (start < dataStream.size && !found) {
+        val result = findDuplicates(dataStream, start, windowSize)
+        found = result < 0
+        if (!found)
+            start += (result - start) + 1 // Move the window to just past the duplicate value
+    }
+
+    print ("Problem " + problemNum + ": Start of stream is at position: " + (start + windowSize) + " with marker values: ")
+    for (i in start until start+windowSize) {
+        print(dataStream[i])
+    }
+
+    println()
+
+}
+fun findDuplicates(dataStream: CharArray, start: Int, windowSize: Int): Int {
+    val letterMap: HashMap<Char, Int> = HashMap<Char, Int> ()
+    var index = start
+    var dupeFound = false
+    do {
+        val currentLetter: Char = dataStream[index]
+        dupeFound = letterMap.containsKey(currentLetter)
+        // if we find a duplicate we should just skip out and return where the first
+        // letter occurred so we can shift the window past the duplicate letter.
+        if (dupeFound) {
+            return letterMap.get(currentLetter)!!
+        }
+        letterMap.put(currentLetter, index)
+        index++
+    } while (index < start + windowSize)
+    return -1 // this indicates we had a clean scan
 }
