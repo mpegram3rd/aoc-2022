ce0736b588f5869706118ca0afcb3fed0a2c21e5
Macon Pegram <mpegram@captechconsulting.com>
2022-12-10 11:04:44 -0500

    Comments added and final tidying up for problem 1

--- 10/cpu.py
+++ 10/cpu.py
@@ -14,26 +14,30 @@ class CPU:
     def read_x_register(self):
         return self._x
 
+    # What cycle is the CPU currently on
     def get_current_cycle(self):
         return self._current_cycle
 
+    # Find out the internal values captured at each breakpoint
     def get_breakpoint_values(self):
         return self._breakpoint_values
 
+    # Noop does nothing for 1 CPU cycle
     def __noop(self, instruction):
         print("Executing: noop")
         self.__tick(1)
 
+    # Add X will add the value in the instruction to the X register after 2 CPU cycles
     def __add_x(self, instruction):
         print("Executing: addx %d" % (int(instruction[1])))
         self.__tick(2)
         self._x += int(instruction[1])
 
+    # Simulates the CPU cycle and captures the current values if a breakpoint is crossed
     def __tick(self, count):
         if count > 0:
             self._current_cycle += 1
             if self._current_cycle in self._breakpoints:
-                print("-- Breakpoint Value for cycle %d with regX %d = %d" % (self._current_cycle, self.read_x_register(), (self._current_cycle * self.read_x_register())))
                 self._breakpoint_values[self._current_cycle] = self.read_x_register()
             self.__tick(count -1)
 
--- 10/main.py
+++ 10/main.py
@@ -1,18 +1,22 @@
 import cpu
 
+# read all the data in at once
 file1 = open('input.txt', 'r')
 Lines = file1.readlines()
+
+# initializes our CPU with the designated breakpoints
 computer = cpu.CPU(1, (20, 60, 100, 140, 180, 220))
 
+# executes each commands
 for line in Lines:
     computer.execute(line)
 
 print("\nBreakpoint Analysis")
 print("Value of X Register at cycle %d: %d" % (computer.get_current_cycle(), computer.read_x_register()))
 
-sum = 0
+total = 0
 breakpoint_values = computer.get_breakpoint_values()
 for value in breakpoint_values:
-    sum += (value * breakpoint_values[value])
+    total += (value * breakpoint_values[value])
 
-print("Sum of values at each breakpoint = %d" % sum)
+print("Sum of values at each breakpoint = %d" % total)
